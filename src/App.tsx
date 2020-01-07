8479b157ac01e786def577dc50b62d208b2018c8
junki <jun34530811@gmail.com>
2020-01-08 00:17:19 +0900

    icon image list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, useMemo} from 'react';
 import './App.css';
+import axios from 'axios';
+import {champions, champion} from 'riot-api-json-ts-wrapper/src/champion';
+
+const url = "http://ddragon.leagueoflegends.com/cdn/9.24.2/data/en_US/champion.json";
+const imageUrl = "http://ddragon.leagueoflegends.com/cdn/9.24.2/img/champion/";
 
 const App: React.FC = () => {
+  
+  useEffect(() => {
+    const getChampions = async() => {
+      const result = await axios.get(url);
+      const champions: champions = result.data;
+      setChampions(champions);
+    }
+    getChampions();
+  }, [])
+
+  const [champions, setChampions] = useState<champions>({
+    type: "",
+    format: "",
+    version: "",
+    data: {},
+  });
+
+  const [filterInput, setFilterInput] = useState<string>("");
+
+  const images = useMemo(() => {
+    return Object.values(champions.data)
+      .filter((c: champion) => {
+        return c.name.toLowerCase().indexOf(filterInput.toLowerCase()) > -1
+      })
+      .map((c: champion) => {
+        const imgUrl = imageUrl + c.image.full;
+        const imgKey = c.id;
+        return (
+            <img src={imgUrl} key={imgKey}/>
+        )
+      });
+    }, [champions, filterInput]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input 
+          type="text"
+          onChange={e => setFilterInput(e.target.value)}
+        />
+      </div>
+      {images}
     </div>
   );
 }
