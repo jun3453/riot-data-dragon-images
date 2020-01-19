1d4810ea874a4efb89a6f0c44f1a80cb6da94af5
junki <jun34530811@gmail.com>
2020-01-19 21:24:44 +0900

    select language & version

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
-import { champions, champion, getChampions, createChampionImageUrl } from 'riot-api-json-ts-wrapper/dist/champion';
+import { champions, champion, getChampions, createChampionImageUrl } from 'riot-api-json-ts-wrapper/dist/DataDragon/champion';
+import { version, getVersions } from 'riot-api-json-ts-wrapper/dist/DataDragon/version';
+import { language, getLanguages } from 'riot-api-json-ts-wrapper/dist/DataDragon/language';
 
 const App: React.FC = () => {
 
+  const [versions, setVersions] = useState<version[]>([]);
+  const [languages, setLanguages] = useState<language[]>([]);
+  const [currentVersion, setCurrentVersion] = useState<version>("");
+  const [currentLanguage, setCurrentLanguage] = useState<language>("");
+
   useEffect(() => {
-    getChampions().then((c: champions) => {
-      setChampions(c);
-    });
+    let version: version = "";
+    let language: language = "";
+    getVersions().then((v: version[]) => {
+      setVersions(v);
+      setCurrentVersion(v[0]);
+      version = v[0];
+      console.log(version);
+    })
+
+    getLanguages().then((l: language[]) => {
+      setLanguages(l)
+      setCurrentLanguage(l[0]);
+      language = l[0];
+    })
   }, [])
+
+  useEffect(() => {
+    if (currentVersion != "" && currentLanguage != "") {
+      getChampions(currentVersion, currentLanguage).then((c: champions) => {
+        setChampions(c);
+      });
+    }
+  }, [currentVersion, currentLanguage])
 
   const [champions, setChampions] = useState<champions>({
     type: "",
@@ -16,7 +42,6 @@ const App: React.FC = () => {
     version: "",
     data: {},
   });
-
   const [filterInput, setFilterInput] = useState<string>("");
 
   const images = useMemo(() => {
@@ -25,7 +50,7 @@ const App: React.FC = () => {
         return c.name.toLowerCase().indexOf(filterInput.toLowerCase()) > -1
       })
       .map((c: champion) => {
-        const imgUrl = createChampionImageUrl(c);
+        const imgUrl = createChampionImageUrl(c, currentVersion);
         const imgKey = c.id;
         return (
           <img src={imgUrl} key={imgKey} />
@@ -33,9 +58,45 @@ const App: React.FC = () => {
       });
   }, [champions, filterInput]);
 
+  const versionsPullDown = useMemo(() => {
+    const options = versions.map((v: version) => {
+      return (
+        <option key={v} value={v}>
+          {v}
+        </option>
+      );
+    })
+    return (
+      <select
+        onChange={(e) => setCurrentVersion(e.target.value)}
+      >
+        {options}
+      </select>
+    );
+  }, [versions]);
+
+  const languagesPullDown = useMemo(() => {
+    const options = languages.map((l: language) => {
+      return (
+        <option key={l} value={l}>
+          {l}
+        </option>
+      );
+    })
+    return (
+      <select
+        onChange={(e) => setCurrentLanguage(e.target.value)}
+      >
+        {options}
+      </select>
+    );
+  }, [languages]);
+
   return (
     <div className="App">
       <div>
+        {versionsPullDown}
+        {languagesPullDown}
         <input
           type="text"
           onChange={e => setFilterInput(e.target.value)}
